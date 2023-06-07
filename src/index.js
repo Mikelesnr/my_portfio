daa6fd75824fc1d361e8ec069d53187a6b685327
Mikelesnr <mwanza.n.m@gmail.com>
2023-06-07 23:54:18 +0200

    flie mods

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // styles
-import "bootstrap/scss/bootstrap.scss";
 import "assets/scss/paper-kit.scss?v=1.3.0";
 import "assets/demo/demo.css?v=1.3.0";
 // pages
